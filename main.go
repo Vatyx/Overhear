cbe76494fc4d442f365aa035ad4f67dd931397ad
Sahil Dhanju <sahildhanju1@gmail.com>
2016-10-15 00:39:03 -0500

    added audio

--- main.go
+++ main.go
@@ -4,7 +4,32 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    for {
+	    messageType, p, err := conn.ReadMessage()
+	    if err != nil {
+	        return
+	    }
+	    if err = conn.WriteMessage(messageType, p); err != nil {
+	        return err
+    }
+}
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t := template.New("index")
@@ -22,6 +47,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fs := http.FileServer(http.Dir("public"));
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
+	http.HandleFunc("/echo", echoHandler)
 	http.HandleFunc("/", index)
 
 	log.Println("Listening on port 3000...")
