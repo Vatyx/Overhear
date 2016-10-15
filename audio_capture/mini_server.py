b13fd4f8a067ec5f01aab0463a3c22e29332b782
tylerjdurden <durden@tamu.edu>
2016-10-15 13:11:57 -0500

    Beginning work with Electron

--- audio_capture/mini_server.py
+++ audio_capture/mini_server.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from threading import Thread
 import sys
 from audio_capture import AudioCapturer
 from websocket import *
 from base64 import b64encode
+import logging
+
+# log everything to stdout
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+# end logging stuff
 
 app = Flask(__name__)
+TEMPLATES_AUTO_RELOAD = True
 
 ac = None
 websocket_client = None
@@ -21,9 +34,19 @@ def process_callback(data):
     count += 1
 
 
+@app.route('/js/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    directory = os.path.join(root_dir, 'static', 'js')
+    print 'directory is: ', directory
+    return send_from_directory(directory, filename)
+
+
 @app.route('/')
 def main():
-    return "Running now..."
+    # return "Running now..."
+    print "Running now..."
+    return render_template('layout.html')
 
 
 @app.route('/run')
@@ -42,18 +65,24 @@ def run():
 @app.route('/pause')
 def pause():
     global ac
-    ac.pause()
-    return 'pause'
+    if ac is not None:
+        ac.pause()
+    else:
+        print 'AudioCapturer must be running before attempting to pause.'
+    return render_template('layout.html')
 
 
 @app.route('/stop')
 def stop():
     global ac, websocket_client
-    ac.stop()
-    websocket_client.close()
+    if ac is not None and websocket_client is not None:
+	ac.stop()
+    else:
+        print 'AudioCapturer must be running before attempting to stop.'
+
     ac = None
     websocket_client = None
-    return 'stop'
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
