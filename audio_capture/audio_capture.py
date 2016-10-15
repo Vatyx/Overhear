c99e109769df53c5d415cc6b4cc861476d8c59ab
Juliang0705 <juliang0705@gmail.com>
2016-10-15 02:15:52 -0500

    reorganize audio_capture class and make it more flexible for future integration

--- audio_capture/audio_capture.py
+++ audio_capture/audio_capture.py
@@ -1,38 +1,67 @@
 import pyaudio
 import wave
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+class AudioCapturer(object):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+    def __init__(self):
+        self.frames = []
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=AudioCapturer.FORMAT,
+                                  channels=AudioCapturer.CHANNELS,
+                                  rate=AudioCapturer.RATE, input=True,
+                                  frames_per_buffer=AudioCapturer.CHUNK,
+                                  stream_callback=lambda data, frame_count, time_info, status: self._callback(data))
+        self.process_callback = None
 
-print("* recording")
+    def _callback(self, data):
+        if self.process_callback:
+            self.process_callback(data)
+        else:
+            self.default_callback(data)
+        return data, pyaudio.paContinue
 
-frames = []
+    def default_callback(self, data):
+        self.frames.append(data)
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    def run(self, process_callback=None):
+        self.process_callback = process_callback
+        self.stream.start_stream()
 
-print("* done recording")
+    def pause(self):
+        self.stream.stop_stream()
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    def save_audio_file(self, filename, frames=None):
+        if not frames:
+            frames = self.frames
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(AudioCapturer.CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(AudioCapturer.FORMAT))
+        wf.setframerate(AudioCapturer.RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+
+def test():
+    import time
+    AC = AudioCapturer()
+    f = []
+    AC.run(process_callback=lambda data: f.append(data))
+    print "recording..."
+    for i in xrange(1, 5):
+        time.sleep(1)
+    AC.stop()
+    print "done..."
+    AC.save_audio_file('output.wav', f)
+
+if __name__ == '__main__':
+    test()
