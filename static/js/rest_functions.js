fd3ae0413ae3554fecaea46bd3001c7806ca90bc
tylerjdurden <durden@tamu.edu>
2016-10-15 22:08:55 -0500

    Some more electron features

--- static/js/rest_functions.js
+++ static/js/rest_functions.js
@@ -1,25 +1,22 @@
-function playpause(){
-	// //POST to the /pause endpoint
-	// var http = new XMLHttpRequest();
-	// //Send the proper header information along with the request
-	// var url = "/pause"
-	// http.open("POST", url, true);
-	// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	// http.onreadystatechange = function() {
-	// 	//Call a function when the state changes.
-	// 	if(http.readyState == 4 && http.status == 200) {
-	// 		alert(http.responseText);
-	// 	}
-	// }
-	// http.send();
-	
-	// var $form = $('form');
-	// $form.submit(function(){
-	// 	$.post($(this).attr('action'), $(this).serialize(), function(response){
-	// 		// do something here on success
-	// 		console.log('Pause success!!\n\n');
-	// 	},'json');
-	// 	return false;
-	// });
+function pauseplayButton() {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			console.log(xhr.responseText);
+		}
+	}
+	xhr.open('GET', '/pause', true);
+	xhr.send(null);
 }
+
+function stopButton() {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			console.log(xhr.responseText);
+		}
+	}
+	xhr.open('GET', '/stop', true);
+	xhr.send(null);
+}
+
